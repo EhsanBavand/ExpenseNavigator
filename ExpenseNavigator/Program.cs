134c7e790be373d4f6e88768824e081b22bca4f8
em125 <ehsanbavandsavadkohei@gmail.com>
2024-01-29 23:32:29 -0500

    Add project files.

---
+++ ExpenseNavigator/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ExpenseNavigator.Data;
+using ExpenseNavigator.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ExpenseNavigatorDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ExpenseNavigatorDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ExpenseNavigatorDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ExpenseNavigatorDbContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// add service to support resister
+builder.Services.AddRazorPages();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireUppercase = false;
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
